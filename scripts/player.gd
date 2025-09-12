bf6d52740f96d39908c8f28aeb9e50815aa9343b
Lucas Sutherland <sutherlandlucas1@gmail.com>
2025-09-12 19:54:47 +1000

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+
+@onready var camera_pivot = $CameraPivot
+
+@export var sensitivity = 0.5
+const SPEED = 7.0
+const JUMP_VELOCITY = 6
+var paused = false
+
+func _ready():
+	Input.mouse_mode =Input.MOUSE_MODE_CAPTURED
+	
+	
+func _input(event):
+	if event is InputEventMouseMotion and !paused:
+		rotate_y(deg_to_rad(event.relative.x * -sensitivity))
+		camera_pivot.rotate_x(deg_to_rad(event.relative.y * -sensitivity))
+		camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, -PI / 2, PI / 8)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	
+	if Input.is_action_just_pressed("pause"):
+		if paused:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		paused = !paused
+			
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
